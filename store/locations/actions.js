158d5fc49bd901020920471041d7602c4cf44990
Guilherme Gomes <guiga.gomes10@gmail.com>
2021-02-05 22:16:07 -0300

    Ajuste pra pegar as localizações

--- store/locations/actions.js
+++ store/locations/actions.js
@@ -4,14 +4,9 @@ export default {
       const response = await this.$axios.get(
         `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat.toString()},${userLng.toString()}&radius=25000&type=gas_station&key=AIzaSyAZN6B6vvnFIUT7ySIbOVjsOuiCVXtbh6M`
       );
-      console.log(response);
-      response.data.results.sort(function(a, b) {
-        return a - b;
-      });
       commit("SET_LOCATIONS", response.data.results);
     } catch (err) {
-      console.log(err);
-      console.log("erro ao pegar mapa");
+      alert("Ocorreu um erro ao carregar o mapa. Por favor, atualize a página");
     }
   }
 };
