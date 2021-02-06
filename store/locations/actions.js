d4c7e321c09bb97b846987ed700182810906b23d
Guilherme Gomes <guiga.gomes10@gmail.com>
2021-02-05 22:17:55 -0300

    Ajuste no mapa

--- store/locations/actions.js
+++ store/locations/actions.js
@@ -2,7 +2,7 @@ export default {
   async GETLOCATIONS({ commit }, { userLat, userLng }) {
     try {
       const response = await this.$axios.get(
-        `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat.toString()},${userLng.toString()}&radius=25000&type=gas_station&key=AIzaSyAZN6B6vvnFIUT7ySIbOVjsOuiCVXtbh6M`
+        `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${userLat.toString()},${userLng.toString()}&radius=25000&type=gas_station&key=AIzaSyBCLd7m25rmxAEslVigBB42kNchGcoz0Mg`
       );
       commit("SET_LOCATIONS", response.data.results);
     } catch (err) {
